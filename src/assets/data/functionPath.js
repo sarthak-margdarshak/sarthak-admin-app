c17ea54cd4b3ed68826ea7fed0daaa4fa9047608
Ritesh Ranjan <riteshranjank2672@gmail.com>
2025-01-31 23:07:24 +0530

    asset-data-complete

---
+++ src/assets/data/functionPath.js
@@ -0,0 +1,12 @@
+export const sarthakAPIPath = {
+  fetch: {
+    user: {
+      list: "/fetch/user/path",
+    },
+  },
+  publish: {
+    question: "/publish/question",
+    mockTest: "/publish/mockTest",
+    product: "/publish/product",
+  },
+};
