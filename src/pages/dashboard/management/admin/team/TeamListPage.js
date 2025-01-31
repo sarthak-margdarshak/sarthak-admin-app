6e9ff5b7daed05f28a45e1aa7040c9bec6da0d4c
Ritesh Ranjan <riteshranjank2672@gmail.com>
2025-02-01 00:04:38 +0530

    team-page

--- src/pages/dashboard/management/admin/team/TeamListPage.js
+++ src/pages/dashboard/management/admin/team/TeamListPage.js
@@ -1,101 +1,16 @@
-import { Link as RouterLink, useNavigate } from "react-router-dom";
-import React, { useCallback, useEffect, useState } from "react";
+import { Fragment } from "react";
 import { Helmet } from "react-helmet-async";
-import {
-  Container,
-  Button,
-  Card,
-  LinearProgress,
-  TableContainer,
-  Table,
-  TableBody,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  TextField,
-  DialogContentText,
-  DialogActions,
-} from "@mui/material";
+import { Container } from "@mui/material";
 import { PATH_DASHBOARD } from "routes/paths";
 import { useSettingsContext } from "components/settings";
 import CustomBreadcrumbs from "components/custom-breadcrumbs";
-import Iconify from "components/iconify/Iconify";
-import { useSnackbar } from "components/snackbar";
-import { useAuthContext } from "auth/useAuthContext";
-import {
-  useTable,
-  getComparator,
-  emptyRows,
-  TableEmptyRows,
-  TableHeadCustom,
-  TablePaginationCustom,
-} from "components/table";
-import Scrollbar from "components/scrollbar";
-import TeamTableRow from "sections/@dashboard/management/admin/team/list/TeamTableRow";
-import { appwriteTeams } from "auth/AppwriteContext";
-import { ID, Query } from "appwrite";
-
-const TABLE_HEAD = [
-  { id: "sn" },
-  { id: "name", label: "Name", align: "left" },
-  { id: "createdAt", label: "Created At", align: "left" },
-  { id: "totalMember", label: "Total Member", align: "left" },
-  { id: "id", label: "Id", align: "left" },
-  { id: "view", label: "View", align: "left" },
-];
+import UsersListComponent from "sections/@dashboard/management/admin/team/UsersListComponent";
 
 export default function TeamListPage() {
-  const navigate = useNavigate();
-
   const { themeStretch } = useSettingsContext();
-  const { enqueueSnackbar } = useSnackbar();
-  const { userProfile } = useAuthContext();
-  const [createTeam, setCreateTeam] = useState(false);
-  const [myTeam, setMyTeam] = useState([]);
-  const [update, setUpdate] = useState(true);
-  const [createTeamOpen, setCreateTeamOpen] = useState(false);
-
-  const {
-    page,
-    order,
-    orderBy,
-    rowsPerPage,
-    //
-    onSort,
-    onChangePage,
-    onChangeRowsPerPage,
-  } = useTable();
-
-  const fetchData = useCallback(async () => {
-    setUpdate(true);
-    try {
-      // Get My Team Data
-      const team = await appwriteTeams.list([Query.limit(100)]);
-      setMyTeam(team.teams);
-      setCreateTeam(userProfile.createTeam);
-    } catch (error) {
-      enqueueSnackbar(error.message, { variant: "error" });
-    }
-    setUpdate(false);
-  }, [userProfile.createTeam, enqueueSnackbar]);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
-  const denseHeight = 72;
-
-  const handleViewRow = (id) => {
-    navigate(PATH_DASHBOARD.team.view(id));
-  };
-
-  const dataFiltered = applyFilter({
-    inputData: myTeam,
-    comparator: getComparator(order, orderBy),
-  });
 
   return (
-    <React.Fragment>
+    <Fragment>
       <Helmet>
         <title>Team: List | Sarthak Admin</title>
       </Helmet>
@@ -105,146 +20,12 @@ export default function TeamListPage() {
           heading="Teams"
           links={[
             { name: "Dashboard", href: PATH_DASHBOARD.root },
-            { name: "Teams" },
+            { name: "Team" },
           ]}
-          action={
-            createTeam && (
-              <Button
-                component={RouterLink}
-                onClick={() => setCreateTeamOpen(true)}
-                variant="contained"
-                startIcon={<Iconify icon="eva:plus-fill" />}
-              >
-                Create A Team
-              </Button>
-            )
-          }
         />
 
-        <Card>
-          <TableContainer sx={{ position: "relative", overflow: "unset" }}>
-            <Scrollbar>
-              {update ? (
-                <LinearProgress />
-              ) : (
-                <Table size={"medium"} sx={{ minWidth: 800 }}>
-                  <TableHeadCustom
-                    order={order}
-                    orderBy={orderBy}
-                    headLabel={TABLE_HEAD}
-                    onSort={onSort}
-                  />
-
-                  <TableBody>
-                    {dataFiltered
-                      .slice(
-                        page * rowsPerPage,
-                        page * rowsPerPage + rowsPerPage
-                      )
-                      .map((row) => {
-                        return (
-                          <TeamTableRow
-                            key={row.$id}
-                            index={row?.sn}
-                            row={row}
-                            onViewRow={() => handleViewRow(row?.$id)}
-                          />
-                        );
-                      })}
-
-                    <TableEmptyRows
-                      height={denseHeight}
-                      emptyRows={emptyRows(
-                        page,
-                        rowsPerPage,
-                        dataFiltered.length
-                      )}
-                    />
-                  </TableBody>
-                </Table>
-              )}
-            </Scrollbar>
-          </TableContainer>
-
-          <TablePaginationCustom
-            count={dataFiltered.length}
-            page={page}
-            rowsPerPage={rowsPerPage}
-            onPageChange={onChangePage}
-            onRowsPerPageChange={onChangeRowsPerPage}
-          />
-        </Card>
+        <UsersListComponent />
       </Container>
-
-      <Dialog
-        maxWidth="md"
-        open={createTeamOpen}
-        PaperProps={{
-          component: "form",
-          onSubmit: async (event) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries(formData.entries());
-            const newTeamName = formJson.newTeamName;
-            try {
-              await appwriteTeams.create(ID.unique(), newTeamName);
-              enqueueSnackbar("Team Created Successfully");
-              fetchData();
-              setCreateTeamOpen(false);
-            } catch (error) {
-              enqueueSnackbar(error.message, { variant: "error" });
-            }
-          },
-        }}
-      >
-        <DialogTitle sx={{ pb: 2 }}>Create Team</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Please enter the name of the team, which you wants to create
-          </DialogContentText>
-          <TextField
-            name="newTeamName"
-            margin="dense"
-            fullWidth
-            autoFocus
-            required
-            placeholder="Enter Name"
-            label="Team Name"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={() => {
-              setCreateTeamOpen(false);
-            }}
-          >
-            Cancel
-          </Button>
-          <Button type="submit">Create</Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
+    </Fragment>
   );
-}
-
-function applyFilter({ inputData, comparator }) {
-  const stabilizedThis = inputData.map((el, index) => [el, index]);
-
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-
-  inputData = stabilizedThis.map((el) => el[0]);
-  const filtered = [];
-  let count = 1;
-  for (let i in inputData) {
-    filtered.push({
-      ...inputData[i],
-      sn: count,
-    });
-    count++;
-  }
-  return filtered;
 }
