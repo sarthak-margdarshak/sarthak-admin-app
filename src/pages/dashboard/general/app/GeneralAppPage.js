a91cd4e0fb77658929e83bfd9291efd2c1025991
Ritesh Ranjan <riteshranjank2672@gmail.com>
2025-02-01 00:04:02 +0530

    general-app

--- src/pages/dashboard/general/app/GeneralAppPage.js
+++ src/pages/dashboard/general/app/GeneralAppPage.js
@@ -1,40 +1,18 @@
-/**
- * Written By - Ritesh Ranjan
- * Website - https://sagittariusk2.github.io/
- *
- *  /|||||\    /|||||\   |||||||\   |||||||||  |||   |||   /|||||\   ||| ///
- * |||        |||   |||  |||   |||     |||     |||   |||  |||   |||  |||///
- *  \|||||\   |||||||||  |||||||/      |||     |||||||||  |||||||||  |||||
- *       |||  |||   |||  |||  \\\      |||     |||   |||  |||   |||  |||\\\
- *  \|||||/   |||   |||  |||   \\\     |||     |||   |||  |||   |||  ||| \\\
- *
- */
-
-// IMPORT ---------------------------------------------------------------
-
 import { Helmet } from "react-helmet-async";
-// @mui
 import { Container } from "@mui/material";
-// auth
-// components
-import { useSettingsContext } from "../../../../components/settings";
-
-// assets
-import React from "react";
-
-// ----------------------------------------------------------------------
+import { useSettingsContext } from "components/settings";
+import { Fragment } from "react";
 
 export default function GeneralAppPage() {
   const { themeStretch } = useSettingsContext();
 
   return (
-    <React.Fragment>
+    <Fragment>
       <Helmet>
         <title> General: App | Sarthak Admin</title>
       </Helmet>
 
-      <Container maxWidth={themeStretch ? false : "xl"}>
-      </Container>
-    </React.Fragment>
+      <Container maxWidth={themeStretch ? false : "xl"}></Container>
+    </Fragment>
   );
 }
