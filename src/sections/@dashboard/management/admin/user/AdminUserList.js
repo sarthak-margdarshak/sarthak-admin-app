beed1e1bf6f4cdf20c6c9bc69f8ec20f97f96036
Ritesh Ranjan <riteshranjank2672@gmail.com>
2025-02-11 01:53:22 +0530

    admin-section-complete

--- src/sections/@dashboard/management/admin/user/AdminUserList.js
+++ src/sections/@dashboard/management/admin/user/AdminUserList.js
@@ -1,9 +1,8 @@
-import {Autocomplete, TextField} from "@mui/material";
-import {useState} from "react";
-import {User} from "auth/User";
-import {appwriteDatabases} from "auth/AppwriteContext";
-import {APPWRITE_API} from "config-global";
-import {Query} from "appwrite";
+import { Autocomplete, TextField } from "@mui/material";
+import { useState } from "react";
+import { appwriteDatabases } from "auth/AppwriteContext";
+import { APPWRITE_API } from "config-global";
+import { Query } from "appwrite";
 
 export default function AdminUserList({ onChange }) {
   const [isUserListLoading, setIsUserListLoading] = useState(false);
@@ -18,8 +17,8 @@ export default function AdminUserList({ onChange }) {
       onFocus={async (event, value) => {
         setIsUserListLoading(true);
         try {
-          const tem = await User.getUserList(value);
-          setUserList(tem);
+          // const tem = await User.getUserList(value);
+          // setUserList(tem);
         } catch (error) {
           console.log(error);
         }
@@ -28,30 +27,33 @@ export default function AdminUserList({ onChange }) {
       onInputChange={async (event, value) => {
         setIsUserListLoading(true);
         try {
-          const tem = await User.getUserList(value);
-          setUserList(tem);
+          // const tem = await User.getUserList(value);
+          // setUserList(tem);
         } catch (error) {
           console.log(error);
         }
         setIsUserListLoading(false);
       }}
       onChange={async (event, value) => {
-        const id = (await appwriteDatabases.listDocuments(
-          APPWRITE_API.databaseId,
-          APPWRITE_API.collections.adminUsers,
-          [Query.select("$id"), Query.equal("empId", value.match(/\w{3}\d{4}/g))]
-        )).documents[0].$id
-        onChange(id)
+        const id = (
+          await appwriteDatabases.listDocuments(
+            APPWRITE_API.databaseId,
+            APPWRITE_API.collections.adminUsers,
+            [
+              Query.select("$id"),
+              Query.equal("empId", value.match(/\w{3}\d{4}/g)),
+            ]
+          )
+        ).documents[0].$id;
+        onChange(id);
       }}
       renderOption={(props, option) => (
         <li {...props} key={props.key}>
           {option}
         </li>
       )}
-      renderInput={(params) => (
-        <TextField {...params} label="Users" />
-      )}
+      renderInput={(params) => <TextField {...params} label="Users" />}
       sx={{ mt: 2 }}
     />
-  )
+  );
 }
